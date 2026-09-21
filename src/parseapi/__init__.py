cc31eaf82cd82fa3b21c3d5dbc27237428cf194a
Billy O'Shall <billy@oshall.com>
2026-09-21 13:39:16 -0400

    Prepare SDK 1.3.0 with Stack inventory support

--- src/parseapi/__init__.py
+++ src/parseapi/__init__.py
@@ -2,5 +2,5 @@
 
 from ._client import AsyncParseAPI, ParseAPI, ParseAPIError
 
-__version__ = "1.1.0"
+__version__ = "1.3.0"
 __all__ = ["ParseAPI", "AsyncParseAPI", "ParseAPIError", "__version__"]
